fbe78b30cd7b32a4e4b3c758b7c760076b737a17
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2025-12-24 17:46:30 +0300

    Added operations with todos

--- src/store/useTodosStore.js
+++ src/store/useTodosStore.js
@@ -5,7 +5,23 @@ const useTodosStore = create((set, get) => ({
             {id: crypto.randomUUID(), title: 'todo2', description: '', completed: true}],
 
 
-    
+    addTodo: (title, description='') => {
+        const currentTodos = get().todos
+        set({todos: [...currentTodos, {id: crypto.randomUUID(), title, description, completed: false}]})
+    },
+
+    deleteTodo: (id) => {
+        const currentTodos = get().todos
+        set({todos: currentTodos.filter(todo => (
+            todo.id !== id))})        
+    },
+
+    toggleTodo: (id) => {
+        const currentTodos = get().todos
+        set({todos: currentTodos.map(todo => (
+            todo.id === id ? {...todo, completed: !todo.completed} : todo
+        ))})
+    }
 }))
 
 export default useTodosStore
