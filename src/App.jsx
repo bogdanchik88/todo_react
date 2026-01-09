4cee0195242a0fba8ad63d1c113e7c4bbd07c53b
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-01-09 19:55:35 +0300

    Hotfix in main app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoList from "./components/TodoList"
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-bg font-Yantramanav">
-      <div className="flex flex-col gap-3 w-[90%] max-w-[500px] p-5 bg-card rounded-2xl shadow-lg">
+      <div className="flex flex-col gap-3 w-[90%] max-w-[500px] max-h-[70vh] p-5 bg-card rounded-2xl shadow-lg">
         <InputForm />
         <TodoList />
       </div>
