492a4819b6a0585598b165096e47286b0a11d6f2
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-01-09 18:16:10 +0300

    Setup and testing custom CSS colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import TodoList from "./components/TodoList"
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-100 font-Yantramanav">
-      <div className="flex flex-col gap-3 w-[90%] max-w-[500px] p-5 bg-white rounded-2xl">
+    <div className="min-h-screen flex items-center justify-center bg-bg font-Yantramanav">
+      <div className="flex flex-col gap-3 w-[90%] max-w-[500px] p-5 bg-card rounded-2xl shadow-lg">
         <InputForm />
         <TodoList />
       </div>
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -28,27 +28,28 @@ const InputForm = () => {
       onSubmit={handleSubmit}
       className="flex flex-row gap-12 w-[95%] justify-start"
     >
-      <div className="flex flex-col justify-start gap-2 w-[70%]">
+      <div className="flex flex-col justify-start gap-4 w-[70%] text-lg">
         <div className="flex flex-col gap-1 justify-start">
-          <label htmlFor="title">Title</label>
           <input
             id="title"
             type="text"
             placeholder="Enter title..."
             ref={titleRef}
             required
-            className="border-[1px] border-2 border-black px-4 py-1 rounded-2xl"
+            className="border-2 border-black px-4 py-1 rounded-2xl focus:border-2 outline-none
+              focus:ring-2 focus: ring-red-200 focus:border-red-500
+                transition-colors duration-300"
           />        
         </div>
 
         <div className="flex flex-col gap-1 justify-start">
-          <label htmlFor="description">Description</label>
           <input
             id="description"
             type="text"
             placeholder="Enter description..."
             ref={descriptionRef}
-            className="border-2 border-black px-4 py-2 rounded-2xl"
+            className="border-2 border-border px-4 py-1 rounded-2xl outline-none
+              focus:border-primary"
           />        
         </div>        
       </div>
@@ -56,8 +57,8 @@ const InputForm = () => {
       <button
         type="submit"
         className="cursor-pointer"
-      ><ClipboardPlus className="hover:text-amber-300 hover:bg-cyan-500 p-1 w-10 h-10
-        transition-colors duration-300"/>
+      ><ClipboardPlus className="p-1 text-success size-10
+        transition-colors duration-100"/>
       </button>
     </form>
   )
