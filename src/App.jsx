b9c60d8345bfd3185725125058af6d60ac679cba
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-01-09 03:24:34 +0300

    Markup hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useEffect } from "react"
-import useTodosStore from "./store/useTodosStore"
 import InputForm from "./components/InputForm"
 import TodoList from "./components/TodoList"
 
 function App() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen gap-3">
-        <InputForm/>
-        <TodoList/>
+    <div className="min-h-screen flex items-center justify-center bg-neutral-100">
+      <div className="flex flex-col gap-3 w-[90%] max-w-[500px] p-5 bg-white rounded-2xl">
+        <InputForm />
+        <TodoList />
       </div>
-    </>
+    </div>
   )
 }
+
 
 export default App
