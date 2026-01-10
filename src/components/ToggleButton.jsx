a3c8f3fb483c55a6f4b83156c1f27ac9796bffe2
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-01-10 12:11:39 +0300

    Added cool toggle button

---
+++ src/components/ToggleButton.jsx
@@ -0,0 +1,44 @@
+import { Check, X } from "lucide-react"
+
+const ToggleButton = ({completed, onToggle} = props) => {
+  return (
+    <button
+    onClick={onToggle}
+    className="relative size-5"
+    >
+    {/* Галочка */}
+    <Check
+        className={`
+        absolute inset-0
+        text-success
+        transition-all duration-300 ease-in-out
+        hover:scale-[120%]
+        active:scale-100
+        ${
+            completed
+            ? "opacity-100 scale-100 pointer-events-auto"
+            : "opacity-0 scale-75 pointer-events-none rotate-90"
+        }
+        `}
+    />
+
+    {/* Крест */}
+    <X
+        className={`
+        absolute inset-0
+        text-danger
+        transition-all duration-300 ease-in-out
+        hover:scale-[120%]
+        active:scale-100
+        ${
+            completed
+            ? "opacity-0 scale-75 pointer-events-none rotate-90"
+            : "opacity-100 scale-100 pointer-events-auto"
+        }
+        `}
+    />
+    </button>
+  )
+}
+
+export default ToggleButton
