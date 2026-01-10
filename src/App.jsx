1db4944c9db20147c38bbdad2243822c355779b9
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-01-11 00:42:50 +0300

    Main app and todoList fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-bg font-Yantramanav">
       <div className="flex flex-col gap-3 w-[90%] max-w-[500px] max-h-[70vh] p-5 bg-card rounded-2xl shadow-lg">
         <InputForm />
+
+      {/* Контейнер под список */}
+      <div className="flex-1 overflow-y-auto">
         <TodoList />
+      </div>
       </div>
     </div>
   )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,10 +7,21 @@ const TodoList = () => {
     
   return (
     <div className='flex flex-col gap-2'>
-        {todos.map(todo => (
-            <TodoListItem key={todo.id} id={todo.id} title={todo.title} description={todo.description} completed={todo.completed}/>
-        ))}
-    </div>
+      {todos.length === 0 && 
+        <p className='mx-auto 
+          text-lg 
+          text-primary 
+          my-2'>You have no todos yet!</p>}
+
+      {todos.map(todo => (
+          <TodoListItem 
+            key={todo.id} 
+            id={todo.id} 
+            title={todo.title} 
+            description={todo.description} 
+            completed={todo.completed}/>
+      ))}
+  </div>
   )
 }
 
